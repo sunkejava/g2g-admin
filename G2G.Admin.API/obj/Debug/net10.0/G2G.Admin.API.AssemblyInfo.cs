c4bcbfd649cdb29ffe07a2438326284b8eaf609a
sunke <sunke@example.com>
2026-03-10 15:16:49 +0800

    feat: 完成 5 项功能优化

--- G2G.Admin.API/obj/Debug/net10.0/G2G.Admin.API.AssemblyInfo.cs
+++ G2G.Admin.API/obj/Debug/net10.0/G2G.Admin.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G2G.Admin.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cedfdc8ecaf011c6a49bbd3e83b7fbe6aaf4ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a63cc2d978c12c3db1cfd02d81038a0e693540")]
 [assembly: System.Reflection.AssemblyProductAttribute("G2G.Admin.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G2G.Admin.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
